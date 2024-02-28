4a2832e2e2cea31f66bfa0d9df547c943ab0fcb3
Elifsuesila <159330959+Elifsuesila@users.noreply.github.com>
2024-02-28 22:11:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-meme_dict = {
-            "CRINGE": "Garip ya da utandırıcı bir şey",
-            "LOL": "Komik bir şeye verilen cevap",
-            "ROFL":"bir şakaya karşılık cevap",
-            "SHEESH" : "onaylamamak",
-            "CREEPY" : "korkunç",
-            "AGGRO" : "agresifleşmek/sinirlenmek"
-            }
-            
-            
-word = input("anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!):")
+import discord
+from discord.ext import commands
 
-if word in meme_dict.keys():
-    # Kelime eşleşiyorsa ne yapmalıyız?
-    print(word,"kelimesinin anlamı",meme_dict[word])
-else:
-    print(word,"kelimesinin anlamı bulunamadı")
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='-', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, nasılsın')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("-iyi sen" * count_heh)
+
+bot.run("")
